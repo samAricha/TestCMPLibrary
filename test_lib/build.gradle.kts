60370b696f7502a1458153393a9bd6527058b8cb
samAricha <samaricha001@gmail.com>
2024-04-07 07:45:01 +0300

    updating project to use java 17

--- test_lib/build.gradle.kts
+++ test_lib/build.gradle.kts
@@ -4,13 +4,16 @@ plugins {
     alias(libs.plugins.kotlinMultiplatform)
     alias(libs.plugins.androidLibrary)
     alias(libs.plugins.jetbrainsCompose)
+    id("maven-publish")
+    id("org.jetbrains.dokka") version "1.7.10"
+    id("signing")
 }
 
 kotlin {
     androidTarget {
         compilations.all {
             kotlinOptions {
-                jvmTarget = "11"
+                jvmTarget = "17"
             }
         }
     }
@@ -60,11 +63,116 @@ android {
         }
     }
     compileOptions {
-        sourceCompatibility = JavaVersion.VERSION_11
-        targetCompatibility = JavaVersion.VERSION_11
+        sourceCompatibility = JavaVersion.VERSION_17
+        targetCompatibility = JavaVersion.VERSION_17
     }
     dependencies {
         debugImplementation(libs.compose.ui.tooling)
     }
 }
+
+
+
+val artifact = "rm-kotlin-sdk"
+val pkgUrl = "https://github.com/RevenueMonster/rm-kotlin-sdk"
+val gitUrl = "github.com:RevenueMonster/rm-kotlin-sdk.git"
+
+val dokkaOutputDir = "$buildDir/dokka"
+
+tasks.dokkaHtml {
+    outputDirectory.set(file(dokkaOutputDir))
+}
+
+val dokkaJar by tasks.creating(Jar::class) {
+    group = JavaBasePlugin.DOCUMENTATION_GROUP
+//    description = "Assembles Kotlin docs with Dokka"
+    archiveClassifier.set("javadoc")
+    from(tasks.dokkaHtml)
+}
+
+publishing {
+    repositories {
+        maven {
+            name = "Oss"
+            setUrl {
+                "https://s01.oss.sonatype.org/service/local/staging/deploy/maven2/"
+            }
+            credentials {
+                username = System.getenv("SONATYPE_USERNAME")
+                password = System.getenv("SONATYPE_PASSWORD")
+            }
+        }
+        maven {
+            name = "Snapshot"
+            setUrl { "https://s01.oss.sonatype.org/content/repositories/snapshots/" }
+            credentials {
+                username = System.getenv("SONATYPE_USERNAME")
+                password = System.getenv("SONATYPE_PASSWORD")
+            }
+        }
+    }
+
+    publications {
+        publications.configureEach {
+            if (this is MavenPublication) {
+                artifact(dokkaJar)
+                pom {
+                    name.set(artifact)
+                    description.set("Revenue Monster Kotlin Multiplatform SDK")
+                    url.set(pkgUrl)
+
+                    licenses {
+                        license {
+                            name.set("MIT license")
+                            url.set("https://opensource.org/licenses/MIT")
+                        }
+                    }
+
+                    issueManagement {
+                        system.set("GitHub Issues")
+                        url.set("$pkgUrl/issues")
+                    }
+
+                    developers {
+                        developer {
+                            id.set("si3nloong")
+                            name.set("Lee Sian Loong")
+                            email.set("sianloong90@gmail.com")
+                        }
+                        developer {
+                            id.set("SnorSnor9998")
+                            name.set("Snor")
+                            email.set("snorsnor9998@gmail.com")
+                        }
+                    }
+
+                    scm {
+                        connection.set("scm:git:git://$gitUrl")
+                        developerConnection.set("scm:git:ssh://$gitUrl")
+                        url.set(pkgUrl)
+                    }
+                }
+            }
+        }
+//        create<MavenPublication>("maven") {
+//        withType<MavenPublication> {
+//            groupId = "$group"
+//            artifactId = artifact
+//            version = version
+//            artifact(dokkaJar)
+//        }
+    }
+}
+
+
+if (System.getenv("GPG_PRIVATE_KEY") != null && System.getenv("GPG_PRIVATE_PASSWORD") != null) {
+    signing {
+        useInMemoryPgpKeys(
+            System.getenv("GPG_PRIVATE_KEY"),
+            System.getenv("GPG_PRIVATE_PASSWORD")
+        )
+        sign(publishing.publications)
+    }
+}
+
 
